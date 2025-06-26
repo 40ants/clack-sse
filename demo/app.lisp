002cc7fd3f387f4de211c5a7c69822ec8d81d225
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-06-27 00:44:44 +0300

    Exported a function from demo app.

--- demo/app.lisp
+++ demo/app.lisp
@@ -6,7 +6,8 @@
   (:import-from #:lack/util/writer-stream)
   (:import-from #:clack-sse
                 #:serve-sse)
-  (:nicknames #:clack-sse-demo/app))
+  (:nicknames #:clack-sse-demo/app)
+  (:export #:start-server))
 (in-package #:clack-sse-demo)
 
 
@@ -78,6 +79,7 @@
 
 
 (defun start-server (&key (port 8080))
+  "Starts a demo server showing how does SSE demo work."
   (let ((app (lack:builder
               (:mount "/events"
                       (serve-sse 'server-sent-events-handler))
